5838117a196c80f1bf7382938babc05a5012d306
Leonardo Lima <eng.leolsouza@hotmail.com>
2023-04-23 17:14:19 -0300

    update filter tag to responsive

--- src/components/TagFilter.jsx
+++ src/components/TagFilter.jsx
@@ -1,14 +1,41 @@
+import { useMediaQuery } from "react-responsive";
+
 const TagFilter = ({ handleTagFilter }) => {
+  const isMobile = useMediaQuery({ maxWidth: 1280 });
+
   return (
-    <div className="flex justify-between w-full px-3 ">
-      <button onClick={() => handleTagFilter("")}>TODOS</button>
-      <button onClick={() => handleTagFilter("Assassin")}>ASSASSINOS</button>
-      <button onClick={() => handleTagFilter("Fighter")}>LUTADORES</button>
-      <button onClick={() => handleTagFilter("Mage")}>MAGOS</button>
-      <button onClick={() => handleTagFilter("Marksman")}>ATIRADORES</button>
-      <button onClick={() => handleTagFilter("Support")}>SUPORTES</button>
-      <button onClick={() => handleTagFilter("Tank")}>TANQUES</button>
-    </div>
+    <>
+      {isMobile ? (
+        <div className="flex justify-center w-full px-3 ">
+          <select
+            className="w-2/4 text-center"
+            onChange={(e) => handleTagFilter(e.target.value)}
+          >
+            <option value="">TODOS</option>
+            <option value="Assassin">ASSASSINOS</option>
+            <option value="Fighter">LUTADORES</option>
+            <option value="Mage">MAGOS</option>
+            <option value="Marksman">ATIRADORES</option>
+            <option value="Support">SUPORTES</option>
+            <option value="Tank">TANQUES</option>
+          </select>
+        </div>
+      ) : (
+        <div className="flex justify-between w-full px-3 ">
+          <button onClick={() => handleTagFilter("")}>TODOS</button>
+          <button onClick={() => handleTagFilter("Assassin")}>
+            ASSASSINOS
+          </button>
+          <button onClick={() => handleTagFilter("Fighter")}>LUTADORES</button>
+          <button onClick={() => handleTagFilter("Mage")}>MAGOS</button>
+          <button onClick={() => handleTagFilter("Marksman")}>
+            ATIRADORES
+          </button>
+          <button onClick={() => handleTagFilter("Support")}>SUPORTES</button>
+          <button onClick={() => handleTagFilter("Tank")}>TANQUES</button>
+        </div>
+      )}
+    </>
   );
 };
 
